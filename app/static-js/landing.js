b2959f850f56d1cec19680b34f9cadff5d33badc
laytzehwu <laytzehwu@yahoo.com>
2016-04-17 12:48:13 +0800

    Add ui.router as angular config. The landing template still not yet fired

--- app/static-js/landing.js
+++ app/static-js/landing.js
@@ -6,3 +6,15 @@ let dependency = [
 ];
 
 let app = angular.module('app', dependency);
+app.config(($stateProvider, $urlRouterProvider) => {
+    $urlRouterProvider.otherwise("/landing");
+    $stateProvider
+        .state('landing',{
+            url: "/ladning",
+            templateUrl: "html/landing.html"
+        }) 
+        .state('login', {
+            url: "/login",
+            templateUrl: "html/login.html"
+        });
+});
